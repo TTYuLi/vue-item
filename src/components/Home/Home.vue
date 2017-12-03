f2d783354eeb6f12d0e32c7512cd918827df1a25
ztw <zhangtingwei526@foxmail.com>
2017-12-03 21:27:44 +0800

    home page finished

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+      <!-- 轮播图 -->
+      <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="(pic,index) in pics" :key="index">
+          <a :href="pic.url">
+            <img :src="pic.img" alt="">
+          </a>
+      </mt-swipe-item>
+  
+      </mt-swipe>
+
+
+      <!-- 九宫格 -->
+    <my-ul>
+      <my-li v-for="(router, index) in HRouters" :key="index">
+        <a href="javascript:;">
+          <div :class="'bgimg '+ router.className">{{router.title}}</div>
+        </a>
+      </my-li>
+    </my-ul>
+    </div>
+</template>
+
+<script>
+export default {
+  created(){
+    this.$axios.get("getlunbo").then( res => {
+      console.log(res)
+      this.pics = res.data.message
+    })
+  },
+  data(){
+    return {
+      pics:[],
+      HRouters:[{
+        title:'新闻列表',
+        className:'news'
+      },
+      {
+        title:'图文分享',
+        className:'pic'
+      },
+      {
+        title:'商品列表',
+        className:'goods'
+      },
+      {
+        title:'留言反馈',
+        className:'feedback'
+      },
+      {
+        title:'搜索资讯',
+        className:'search'
+      },
+      {
+        title:'联系我们',
+        className:'callme'
+      }]
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+/*轮播图样式*/
+.mint-swipe-item img {
+    width: 100%;
+ }
+.mint-swipe{
+    height: 30%;
+}
+/*九宫格样式*/
+.bgimg{
+        background-size: 50px;
+        background-repeat: no-repeat;
+        background-position: 40px 35px;
+        font-size: 20px;
+        font-family: 'Microsoft YaHei';
+}
+li a{
+    color:black;
+}
+/* 图片 */
+.news {
+  background-image: url(../../static/img/news.png)
+}
+.pic {
+  background-image: url(../../static/img/picShare.png)
+}
+.goods{
+    background-image: url(../../static/img/goodShow.png)
+}
+.feedback{
+    background-image: url(../../static/img/feedback.png)
+}
+.search{
+    background-image: url(../../static/img/search.png)
+}
+.callme{
+    background-image: url(../../static/img/callme.png)
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -9,9 +9,19 @@ import 'mint-ui/lib/style.css'
 Vue.use(MintUi)
 Vue.use(VueRouter)
 
+//注册全局组件
+import MyLi from './components/common/MyLi.vue'
+import MyUl from './components/common/MyUl.vue'
+Vue.component(MyLi.name,MyLi)
+Vue.component(MyUl.name,MyUl)
+
+
 //引入各种路由组件
 import App from './components/App.vue'
-import Home from './components/Home.vue'
+import Home from './components/Home/Home.vue'
+import Search from './components/Search/Search.vue'
+import Shopcart from './components/Shopcart/Shopcart.vue'
+import Member from './components/Member/Member.vue'
 
 // 配合路由和规则
 const router = new VueRouter()
@@ -20,8 +30,16 @@ router.addRoutes([
   //重定向到首页
   {path:'/',redirect:{name:'home'}}
   ,
-  {name:'home',path:'/home',component:Home}
+  {name:'home',path:'/home',component:Home},
+  {name:'search',path:'/search',component:Search},
+  {name:'shopcart',path:'/shopcart',component:Shopcart},
+  {name:'member',path:'/member',component:Member}
 ])
+
+
+// 引入css资源
+import './static/css/global.css'
+
 
 
 //开启Axios
